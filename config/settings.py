e5f041967708230b16dbfadf3c0f9c581e7e7ef7
Liza Babayan <liza_babayan.ru@mail.ru>
2022-03-18 16:54:17 +0300

    создали приложение app

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,' static'),
-    os.path.jin(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'media')
 ]
 
 STATIC_URL = '/static/'
